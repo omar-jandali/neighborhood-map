8f3bf740265b5b97a20b24246451ad4eeedbfc1c
omar jandali <omar jandali>
2016-08-24 00:03:59 -0700

    added the yelp api - has request error

--- js/main.js
+++ js/main.js
@@ -7,72 +7,76 @@
 /*----------------This is the model---------------------*/
 var initialLocation = {
   center:{
-    lat: 33.933633,
-    lng: -118.095998
+    lat: 34.04129,
+    lng: -118.263667
   },
-  zoom: 11
+  zoom: 14
 }
 
 var stadiums = [
   {
-    title:"Staple Center",
-    address:"1111 S Figueroa St, Los Angeles, CA 90015",
-    teams:[
-      "Los Angeles Lakers",
-      "Los Angeles Clippers",
-      "Los Angeles Kings"
-    ],
+    title:"Maccheroni Republic",
+    city: "Los Angeles",
+    address:"332 South Broadway Los Angeles, CA 90013",
+    foodType: "italian",
     location:{
-      lat: 34.042773,
-      lng: -118.267233
+      lat: 34.050076,
+      lng: -118.248646
     },
     map: map
   },
   {
-    title:"Honda Center",
-    address:"2695 E katella Ave, Anaheim, CA 92806",
-    teams:[
-      "Anaheim Ducks"
-    ],
+    title:"Baco Mercat",
+    city: "Los Angeles",
+    address:"408 South Main Street, Los Angeles, CA 90013",
+    foodType: "spanish-fusion",
     location:{
-      lat: 33.807834,
-      lng: -117.876576
+      lat: 34.047847,
+      lng: -118.247222
     },
     map: map
   },
   {
-    title:"Los Angeles Memorial Coliseum",
-    address:"3911 S Figueroa St. Los Angeles, CA 90037",
-    teams:[
-      "Los Angeles Rams"
-    ],
+    title:"Revolutionario North African Tacos",
+    city: "Los Angeles",
+    address:"1436 West Jefferson Boulevard, Los Angeles, CA 90007",
+    foodType: "north african tacos",
     location:{
-      lat: 34.014049,
-      lng: -118.287945
+      lat: 34.025333,
+      lng: -118.298615
     },
     map: map
   },
   {
-    title:"Dodgers Stadium",
-    address:"1000 Vin Scully Ave, Los Angeles, CA 90012",
-    teams:[
-      "Los Angeles Dodgers"
-    ],
+    title:"Pie Hole",
+    city: "Los Angeles",
+    address:"714 Traction Ave, Los Angeles, CA 90013",
+    foodType: "homemade pie",
     location:{
-      lat: 34.073735,
-      lng: -118.239980
+      lat: 34.045429,
+      lng: -118.236258
     },
     map: map
   },
   {
-    title:"Angels Stadium of Anaheim",
-    address:"2000 E Gene Autry Way, Anaheim, CA 92806",
-    teams:[
-      "Anaheim Angels"
-    ],
+    title:"Stumptown Coffee",
+    city: "Los Angeles",
+    address:"06 S Santa Fe Ave, Los Angeles, CA 90021",
+    foodType: "coffee bar",
     location:{
-      lat: 33.799051,
-      lng: -117.882845
+      lat: 34.033292,
+      lng: -118.229707
+    },
+    map: map
+  },
+  {
+    title:"Mex Peru Gipsy",
+    city: "Los Angeles",
+    address:"414 E 12th St, Los Angeles, CA 90015",
+    foodType: "mexican",
+    location:{
+      lat: 34.035217,
+      lng: -118.255887
     },
     map: map
   }
@@ -90,11 +94,19 @@ var markerList = [];
 
 //The following variables are for streetview request
 var httpsRequest = "https://maps.googleapis.com/maps/api/streetview\?";
-var httpsSize = "size=250x150&";
-var httpsFov = "fov=50&";
+var httpsSize = "size=300x150&";
+var httpsFov = "fov=120&";
 var httpsHeading = "undefined";
-var httpsPitch = "pitch=20&";
+var httpsPitch = "pitch=0&";
 var httpsKey = "key=AIzaSyDik_TF3x4yc96CvWWw12YQ4DMjoG3vfFM";
+var newLocation;
+
+// the following variables are for the yelp api
+var yelpAPI = "https://api.yelp.com/v2/search\?";
+var yelpTermAPI;
+var yelpLocationAPI;
+var newTerm;3
+var customerKey = "oauth_token=zp18SRf0yS1_zN5fmjcL2RFx80t4Vmo-"
 
 /*----------------This is the viewmodel---------------------*/
 function setMarkers(){
@@ -107,8 +119,12 @@ function setMarkers(){
       content: {
         name: stadiums[i].title,
         address: stadiums[i].address,
-        streetViewRequest: httpsRequest + httpsSize + "location=" + stadiums[i].location.lat + "," + stadiums[i].location.lng + "&" + httpsFov + httpsPitch + httpsKey
+        streetLocation: reformatLocation(stadiums[i].address),
+        streetViewRequest: httpsRequest + httpsSize + "location=" + newLocation + "&" + httpsFov + httpsPitch + httpsKey
       },
+      yelpTerm: formatYelpTerm(stadiums[i].title),
+      yelpLocation: formatYelpLocation(stadiums[i].city),
+      yelpRequest: yelpAPI + "id=" + newterm + "&" + customerKey,
       animation: google.maps.Animation.BOUNCE,
       id: i
     });
@@ -123,16 +139,26 @@ function setMarkers(){
 function fillWindow(currentMarker, lInfowindow){
   if(lInfowindow.currentMarker != currentMarker){
     lInfowindow.currentMarker = currentMarker;
-    lInfowindow.setContent("<div><h2>" + currentMarker.content.name + "</h2><h3>" + currentMarker.content.address + "</h3><div>" + currentMarker.content.streetViewRequest + "</div></div>");
+    lInfowindow.setContent("<div><h2>" + currentMarker.content.name + "</h2><h3>" + currentMarker.content.address + "</h3><img src=\"" +
+                            currentMarker.content.streetViewRequest + "\"><img src=\"" + currentMarker.yelpRequest + "\"></div>");
     lInfowindow.open(map, currentMarker);
   }
 }
-/*function infoWindowContent(){
-  var contentString = "<div><h2>"currentMarker.content.name"</h2><h3>"currentMarker.content.address"</h3><div>"contentMarker.content.streetViewRequest"</div></div>";
-  return contentString;
-}*/
+function reformatLocation(address){
+  newLocation = address.replace(/ +/g, "+");
+  console.log(newLocation);
+}
+function formatYelpTerm(title){
+  newterm = title.replace(/ +/g, "-");
+  console.log(newterm);
+}
+function formatYelpLocation(city){
+  newLocation = city.replace(/ +/g, "+");
+  console.log(newLocation);
+}
 
-// sample api https request for locations = https://maps.googleapis.com/maps/api/streetview?size=HEIGHTxWEIGHT&location=LAT,LNG&fov=VALUE&heading=VALUE&pitch=VALUE&key=APIKEY
+// sample api https request for locations ~ https://maps.googleapis.com/maps/api/streetview?size=HEIGHTxWEIGHT&location=LAT,LNG&fov=VALUE&heading=VALUE&pitch=VALUE&key=APIKEY
+// sample wikipedia api requeest https ~ https://en.wikipedia.org/w/api.php?action=query&titles=mainPage
 /*----------------This is the view---------------------*/
 //$(".container.").append(initMap);
 function initMap() {
